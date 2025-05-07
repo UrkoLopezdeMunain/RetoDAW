00be7ba1dd468ec9790352d37c3c7ca015186f48
eathan <eathanandres.garzon@ikasle.egibide.org>
2025-05-07 10:26:13 +0200

    EnfrentamientoDAO + arreglar cosas

--- Reto_Valorant/Programacion/Reto/src/ModeloDAO/EnfrentamientoDAO.java
+++ Reto_Valorant/Programacion/Reto/src/ModeloDAO/EnfrentamientoDAO.java
@@ -4,18 +4,25 @@ import Modelo.Enfrentamiento;
 import Modelo.Equipo;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class EnfrentamientoDAO {
     private static ArrayList<Enfrentamiento> enfrentamientos;
     protected Connection con;
+    protected String sql;
     public EnfrentamientoDAO(Connection c) {
         this.con = c;
     }
 
-    public boolean anadirEnfrentamientos(Enfrentamiento en) {
-        //para la comunicacion con Vista
-        return false;
+    public boolean anadirEnfrentamientos(Enfrentamiento en) throws SQLException {
+        sql = "INSERT INTO enfrentamientos(nombre,fecha_fundacion) VALUES(?,?)";
+        PreparedStatement ps = con.prepareStatement(sql);
+        ps.setString(1, en.getIdEnfrentamiento());
+        ps.setDate(2, en.getResultadosEq1());
+        //antes de llegar a ps.executeUpdate si salta un error entonces devolerá false, en otro caso True
+        return ps.executeUpdate() != 0;
     }
 
     public ArrayList<Enfrentamiento> getEnfrentamientos() {
