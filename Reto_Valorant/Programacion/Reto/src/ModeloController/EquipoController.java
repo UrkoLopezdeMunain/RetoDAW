dbe8265d604f2d08a6b909cc7b32b5e53f4ecfe7
UrkoLopezdeMunain <urko.lopezdemunain@ikasle.egibide.org>
2025-05-09 16:31:07 +0200

    Update EquipoController.java

--- Reto_Valorant/Programacion/Reto/src/ModeloController/EquipoController.java
+++ Reto_Valorant/Programacion/Reto/src/ModeloController/EquipoController.java
@@ -14,21 +14,44 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+
+/**
+ * @author Equipo tres
+ * @version (2.0)
+ */
 public class EquipoController {
 
     private EquipoDAO eDAO;
     //fecha fundacion hay que meterla en Juego
+    /**
+    *Lista interna de equipos
+    */
     private List<Equipo> equipos;
-
+    
+    /**
+     * Constructor
+     *
+     * @param eDAO 
+     */
     public EquipoController(EquipoDAO eDAO) {
         this.eDAO = eDAO;
     }
 
     public EquipoController() {}
 
+    /**
+     * Para decir la fecha de fundacion del juego
+     * @param j jugador
+     */
     public void definirFechaFundacion(Juego j) {
         LocalDate FECHAFUNDACION = j.getFechaSalida();
     }
+
+    /**
+     * Obtiene la lista de todos los equipos 
+     * @return los equipos
+     * @throws SQLException si hay un error 
+     */
     public List<Equipo> getEquipos() throws SQLException {
         equipos = eDAO.getEquipos();
         return equipos;
@@ -38,10 +61,23 @@ public class EquipoController {
     /**Metodos de validacion:
      * Pasa por este metodo validando el nombre y devolviendo el String a validarEquipo()*/
 
+    
+    /**
+     * Valida un equipo 
+     * @param equipo equipo a validar
+     * @return el equipo corectamente
+     * @throws Exception si hay un error 
+     */
     public Equipo validarEquipo(Equipo equipo) throws Exception {
         return eDAO.validarEquipo(equipo);
     }
 
+    /**
+     * Valida que el nombre del equipo cumpla con el patrón permitido, el definido
+     * @param nombre nombre a validar
+     * @return el nombre correctamente
+     * @throws Exception si no cumple con el patrón definido
+     */
     public String validarNombre(String nombre) throws Exception {
         Pattern p = Pattern.compile("^[a-zA-Z0-9][a-zA-Z0-9 _-]{3,15}$"); //15 como mucho como en MER/MR
         Matcher matcher = p.matcher(nombre);
@@ -52,6 +88,12 @@ public class EquipoController {
     }
 
     /**Se pasa a java.sql.Date en DAO*/
+    /**
+     * Valida la fecha cumpla con lo definido
+     * @param fecha  en formato dd-MM-yyyy.
+     * @return fecha válida
+     * @throws Exception si el formato es incorrecto, no como lo definido
+     */
     public LocalDate validarFecha(String fecha) throws Exception {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
         try {
@@ -66,26 +108,65 @@ public class EquipoController {
     }
 
     /**CRUD de EquipoController*/
+    
+    /**
+     * Elimina un equipo 
+     * @param equipo equipo a borrar
+     * @return true si se borró correctamente el equipo
+     * @throws Exception si hay error e
+     */
     public boolean borrarEquipo(Equipo equipo) throws Exception {
         return eDAO.borrarEquipo(equipo);
     }
     /**Al lanzar excepcion no hace falta colocarlos dentro de un 'if' , ya que si algo sale mal directamente relanzarán a la funcion padre*/
+    /**
+     * Crear un nuevo equipo validando su nombre
+     * @param equipo equipo a crear
+     * @return true si se creó corecto
+     * @throws Exception si no se crea correctamente, con la validacio
+     */
     public boolean crearEquipo(Equipo equipo) throws Exception {
         validarNombre(equipo.getNombre());
         return eDAO.crearEquipo(equipo);
     }
+
+    /**
+     * Actualiza la fecha de fundación de un equipo
+     * @param equipo equipo con la fecha actualizada
+     * @return true si se actualiza correctamente
+     * @throws Exception si la validación falla, erronea
+     */
     public boolean actualizarEquipoFecha(Equipo equipo) throws Exception {
         validarNombre(equipo.getNombre());
         return eDAO.actualizarFechaEquipo(equipo);
     }
 
+    /**
+     * Actualiza el nombre del equipo despues de validarlo
+     * @param equipo equipo con nuevo nombre
+     * @return true si se actualiza corectamente
+     * @throws Exception si el nombre es erroneno
+     */
     public boolean actualizarEquipoNombre(Equipo equipo) throws Exception {
         validarNombre(equipo.getNombre());
         return eDAO.actualizarNombreEquipo(equipo);
     }
+
+    /**
+     * Obtiene un equipo teniendo en cuenta su identificador , su id
+     * @param id identificador del equipo
+     * @return equipo 
+     * @throws Exception en caso de no encontrar
+     */
     public Equipo getEquipoPorId(int id) throws Exception{
         return eDAO.getEquipoPorId(id);
     }
+
+    /**
+     * Llama al procedimiento  
+     * @return resultados del procedimiento
+     * @throws SQLException si hay error 
+     */
     public List<String> getEquiposProcedimiento() throws SQLException {
         return eDAO.getEquiposProcedimiento();
     }
