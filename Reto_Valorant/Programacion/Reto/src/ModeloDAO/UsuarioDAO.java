72feb9d7bfaca072eb3eac3b8bddbceaf966b514
UrkoLopezdeMunain <urko.lopezdemunain@ikasle.egibide.org>
2025-05-09 15:35:19 +0200

    Update UsuarioDAO.java
    documentacion

--- Reto_Valorant/Programacion/Reto/src/ModeloDAO/UsuarioDAO.java
+++ Reto_Valorant/Programacion/Reto/src/ModeloDAO/UsuarioDAO.java
@@ -7,16 +7,35 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+
+/**
+ * @author Equipo tres
+ * @version (2.0)
+ * @see Usuario
+ */
 public class UsuarioDAO {
     private String sql;
+    /**
+     * Conexión a la base de datos.
+     */
     protected Connection con;
 
+    /**
+     * Inicia conexion a la base de datos
+     * @param c conexión 
+     */
     public UsuarioDAO(Connection c) {
         this.con = c;
     }
 
     //crud
     /** hecho de esta manera para que pueda lanzar la exception en IniciarSesion.class */
+    /**
+     * Valida un usuario
+     * @param usuario el usuario que se quiere validar 
+     * @return Usuario con los datos obtenidos
+     * @throws SQLException si ocurre un error en la base
+     */
     public Usuario validarUsuario(Usuario usuario) throws SQLException {
         sql="SELECT * FROM usuarios WHERE lower(nombre) = ?";
         PreparedStatement ps = con.prepareStatement(sql);
