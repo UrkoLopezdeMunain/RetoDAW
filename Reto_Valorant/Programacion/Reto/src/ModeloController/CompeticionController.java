b3e7012f1055d07dfc5c3ed3260e742832acdc10
UrkoLopezdeMunain <urko.lopezdemunain@ikasle.egibide.org>
2025-05-09 15:55:43 +0200

    Update CompeticionController.java

--- Reto_Valorant/Programacion/Reto/src/ModeloController/CompeticionController.java
+++ Reto_Valorant/Programacion/Reto/src/ModeloController/CompeticionController.java
@@ -5,16 +5,36 @@ import ModeloDAO.CompeticionDAO;
 
 import java.sql.SQLException;
 
+/**
+ * @author Equipo tres
+ * @version (2.0)
+ * @see Competicion
+ * @see CompeticionDAO
+ */
 public class CompeticionController {
     private final CompeticionDAO competicionDAO;
 
+    /**
+     * El constructor
+     * @param competicionDAO 
+     */
     public CompeticionController(CompeticionDAO competicionDAO) {
         this.competicionDAO = competicionDAO;
     }
 
+    /**
+     * Recupera la competición 
+     * @return competcicon
+     * @throws SQLException si ocurre un error a la base
+     */
     public Competicion getCompeticion() throws SQLException {
         return competicionDAO.conseguirCompeticion();
     }
+
+    /**
+     * Inicio de la competicion
+     * @throws SQLException si ocurre un error en la base
+     */
     public void empezarCompeticion() throws SQLException {
         competicionDAO.empezarCompeticion();
     }
