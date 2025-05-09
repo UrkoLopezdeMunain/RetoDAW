4d3c0d000d0f646306f07eaa1d6587e7659c1e9d
UrkoLopezdeMunain <urko.lopezdemunain@ikasle.egibide.org>
2025-05-09 15:47:12 +0200

    Update EnfrentamientoDAO.java

--- Reto_Valorant/Programacion/Reto/src/ModeloDAO/EnfrentamientoDAO.java
+++ Reto_Valorant/Programacion/Reto/src/ModeloDAO/EnfrentamientoDAO.java
@@ -10,17 +10,39 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
+
+/**
+ * @author Equipo tres
+ * @version (2.0)
+ * @see Enfrentamiento
+ */
 public class EnfrentamientoDAO {
     protected Connection con;
     protected String sql;
     protected EnfrentamientoController enfrentamientoController;
+
+    /**
+     * Inicia la conexion a la base
+     * @param c conexión 
+     */
     public EnfrentamientoDAO(Connection c) {
         this.con = c;
     }
+
+    /**
+     * Asigna el controlador 
+     * @param enfrentamientoController 
+     */
     public void setEnfrentamientoController(EnfrentamientoController enfrentamientoController){
         this.enfrentamientoController = enfrentamientoController;
     }
-
+    
+    /**
+     * Inserta un nuevo enfrentamiento 
+     * @param en enfrentamiento a insertar
+     * @return true si la inserción fue correcta
+     * @throws SQLException en caso de error 
+     */
     public boolean anadirEnfrentamientos(Enfrentamiento en) throws SQLException {
         String sql = "INSERT INTO enfrentamientos (id_enfrentamiento, resultados_eq_1, resultados_eq_2, hora, cod_equ_1, cod_equ_2, num_jornada) " +
                     "VALUES (default, ?, ?, ?, ?, ?, ?)";
@@ -37,7 +59,11 @@ public class EnfrentamientoDAO {
         return si;
     }
 
-
+    /**
+     * Obtiene todos los enfrentamientos almacenados 
+     * @return la lista de los enfrentamientos
+     * @throws Exception si ocurre un error
+     */
     public ArrayList<Enfrentamiento> getEnfrentamientos() throws Exception{
         ArrayList<Enfrentamiento> enfrentamientos = new ArrayList<>();
         String sql = "SELECT * FROM enfrentamientos";
@@ -57,6 +83,13 @@ public class EnfrentamientoDAO {
             }
         return enfrentamientos;
     }
+
+    /**
+     * Obtiener enfrentamientos de una jornada 
+     * @param j número de jornada.
+     * @return la lista de los  enfrentamientos
+     * @throws Exception en caso de error
+     */
     public ArrayList<Enfrentamiento> enfrentamientos(int j) throws Exception{
         ArrayList<Enfrentamiento> enfrentamientos = new ArrayList<>();
         String sql = "SELECT * FROM enfrentamientos where num_jornada = ?";
@@ -77,6 +110,12 @@ public class EnfrentamientoDAO {
         }
         return enfrentamientos;
     }
+    /**
+     * Ejecuta un procedimiento 
+     * @param j número de jornada
+     * @return lista de enfrentamientos 
+     * @throws Exception si ocurre un error
+     */
     public List<String> enfrentamientosProcedimiento(int j) throws Exception {
         ResultSet rs = null;
         String sql = "{call pr_conseguir_info_equipos(?)}";
@@ -106,6 +145,12 @@ public class EnfrentamientoDAO {
         return enfrentamientos;
     }
 
+    /**
+     * Obtiene un enfrentamiento teniendo en cuenat su ID.
+     * @param id identificador del enfrentamiento
+     * @return Enfrentamiento
+     * @throws Exception si no se encuentra o hay algun error
+     */
     public Enfrentamiento getEnfrentamientoPorId(int id) throws Exception{
         String sql = "SELECT * FROM enfrentamientos WHERE id_enfrentamiento = ?";
         PreparedStatement ps = con.prepareStatement(sql);
@@ -126,7 +171,13 @@ public class EnfrentamientoDAO {
 
     }
 
-
+    /**
+     * Para buscar  enfrentamiento entre dos equipos 
+     * @param eq1 para el equipo 1
+     * @param eq2 para el equipo 2
+     * @return enfrentamiento o null
+     * @throws SQLException si hay error 
+     */
     public Enfrentamiento getEnfrentamientoPorEquipos(Equipo eq1, Equipo eq2) throws SQLException{
         String sql = "SELECT * FROM enfrentamientos WHERE cod_equ_1 = ? AND cod_equ_2 = ?";
         PreparedStatement ps = con.prepareStatement(sql);
@@ -146,6 +197,12 @@ public class EnfrentamientoDAO {
             }else
                 return null;
     }
+
+    /**
+     * Actualiza los resultados de un enfrentamiento
+     * @param enfrentamiento 
+     * @throws SQLException si ocurre un error 
+     */
     public void actualizarEnfrentamiento(Enfrentamiento enfrentamiento) throws SQLException {
         sql="UPDATE enfrentamientos SET resultados_eq_1 = ? WHERE id_enfrentamiento = ?";
         PreparedStatement ps = con.prepareStatement(sql);
@@ -158,6 +215,13 @@ public class EnfrentamientoDAO {
         ps.setInt(2, enfrentamiento.getIdEnfrentamiento());
         ps.executeUpdate();
     }
+
+    /**
+     * Obtiene todos los enfrentamientos de una jornada 
+     * @param jornada para el número de jornada
+     * @return enfrentamientos
+     * @throws Exception si ocurre un error
+     */
     public List<Enfrentamiento> getEnfrentamientoPorJornada(int jornada) throws Exception {
         List<Enfrentamiento> enfrentamientos = new ArrayList<>();
         String sql = "SELECT * FROM enfrentamientos WHERE num_jornada = ?";
